d86d54ff07a5faaa2d6004c87c7160e736858648
aditulang <aditulang767@gmail.com>
2022-02-09 15:12:25 -0800

    baruuu tanggal 9

--- src/components/todo-create/TodoCreate.js
+++ src/components/todo-create/TodoCreate.js
@@ -2,23 +2,29 @@ import './TodoCreate.css'
 import { useState } from "react";
 
 const TodoCreate = (props) => {
-    const [getInputTodo ,setInputTodo] = useState('')
+    const [getInputTodo, setInputTodo] = useState('')
+
     const handleSubmit = (event) => {
         event.preventDefault()
 
         const newTodo = {
             id: Math.floor(Math.random() * 100) + 1,
-            title: 'Learn react'
+            title: getInputTodo
         }
 
         props.onCreateTodo(newTodo)
 
-
-        //console.log(newTodo) 
+        setInputTodo('')
     }
+
+    const handleInputTodo = (event) => {
+        setInputTodo(event.target.value) 
+        console.log(getInputTodo) 
+    }
+
     return (
        <form className="todo-form" onSubmit={handleSubmit}>
-           <input type="text" />
+           <input type="text" value={getInputTodo} onChange={handleInputTodo}/>
            <button type="submit">Add</button>
        </form>
     )
